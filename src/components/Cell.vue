d309f753fabd051eaab75f2c2e2dbc70c78c8721
AnwarAsry <anwareducation03@gmail.com>
2025-01-13 17:15:46 +0100

    Grid layout and basic style

---
+++ src/components/Cell.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps(["cell"]);
+</script>
+
+<template>
+    <span
+        class="size-36 flex justify-center items-center border-2 rounded-lg text-8xl font-bold uppercase transition-colors ease-in"
+        :class="{
+            'cursor-default': props.cell !== null,
+            'cursor-pointer hover:bg-blue-50/5': props.cell === null
+        }">
+        <slot></slot>
+    </span>
+</template>
